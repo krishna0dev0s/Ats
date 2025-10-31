cf81fc3d1cc7df1bdcacc70ac66f2460beec3ab1
krishna0dev0s <kg933617@gmail.com>
2025-10-31 14:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import google.generativeai as genai
 # --------------------------
 
 st.sidebar.title("🔑 Gemini API Configuration")
-user_api_key = st.sidebar.text_input("Enter your Gemini API Key", type="password")
+user_api_key = st.sidebar.text_input("AIzaSyCxzV0CnE70klTY_YPcBDyYyi396TQWWRc", type="password")
 
 # Save API key
 if st.sidebar.button("Save API Key"):
